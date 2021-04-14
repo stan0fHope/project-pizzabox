5aa3143a282afc1a146e9eb84dd0c947a77ac951
Stanhope Nwosu <stanhope.nwosu@gmail.com>
2021-04-14 12:59:01 -0400

    Project talk

---
+++ PizzaBox.Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System;
+using sc = System.Console;
+    namespace PizzaBox.Client
+    {//client side (namespace)
+        public class Program //class
+        {
+            private static void Main() //methods
+            {
+                //arrays are static; linked list easy to add, long to search
+                //flex of linked, easy of array => list
+                var stores = new List<string> {"Store 01", "Store 02"};
+                List<string> storeList = new List<string> {"Store 01", "Store 02"};
+                //^var runtime will fill in data-type info
+                //aka var x = null
+
+                for (var x = 0; x < stores.Count; x += 1)
+                {
+                    Console.WriteLine(x + " " + stores[x]);
+                    Console.WriteLine($"{x} {stores[x]}");
+                }
+
+                Console.WriteLine("make a select: ");
+                string input = sc.ReadLine();
+                int entry = int.Parse(input);
+
+                sc.WriteLine(stores[entry]);
+                //focus on process & refining it
+            }
+        }
+    }
