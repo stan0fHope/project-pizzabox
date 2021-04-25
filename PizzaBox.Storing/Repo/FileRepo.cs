e2f8960bef157c433accb82f6f28198448f9cae4
Stanhope Nwosu <stanhope.nwosu@gmail.com>
2021-04-25 18:17:37 -0400

    Remove CompSingleton for now

--- PizzaBox.Storing/Repo/FileRepo.cs
+++ PizzaBox.Storing/Repo/FileRepo.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
+using PizzaBox.Domain;
 using PizzaBox.Domain.Abstracts;
 
 namespace PizzaBox.Storing.Repo
